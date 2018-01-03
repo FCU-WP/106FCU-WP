9110890a74db90e0c1be1e4f8a3c582856ca9ac1
d0409891 <huang351279@gmail.com>
2018-01-03 16:54:51 +0800

    local -> byethost

--- Byet_Host_MySQL.php
+++ Byet_Host_MySQL.php
@@ -2,11 +2,10 @@
 # FileName="Connection_php_mysql.htm"
 # Type="MYSQL"
 # HTTP="true"
-$hostname = "localhost";
+$hostname = "sql301.byethost32.com";
 $database= "b32_21081456_guest";
-$username= "root";
-$password= "082351279";
-mysql_connect($hostname, $username, $password);
-mysql_select_db($database);
+$username= "b32_21081456";
+$password= "0hxpn82y";
+$Byet_MySQL = mysql_pconnect($hostname, $username, $password) or trigger_error(mysql_error(),E_USER_ERROR);
 mysql_query("set names 'utf8'");
 ?>
