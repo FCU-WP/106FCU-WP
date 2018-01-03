70d48842de22011a269a02f4ecb994f93ee1b38d
d0409891 <huang351279@gmail.com>
2018-01-03 16:51:25 +0800

    save date error add type search

--- Byet_Host_MySQL.php
+++ Byet_Host_MySQL.php
@@ -2,10 +2,11 @@
 # FileName="Connection_php_mysql.htm"
 # Type="MYSQL"
 # HTTP="true"
-$hostname = "sql301.byethost32.com";
+$hostname = "localhost";
 $database= "b32_21081456_guest";
-$username= "b32_21081456";
-$password= "0hxpn82y";
-$Byet_MySQL = mysql_pconnect($hostname, $username, $password) or trigger_error(mysql_error(),E_USER_ERROR);
+$username= "root";
+$password= "082351279";
+mysql_connect($hostname, $username, $password);
+mysql_select_db($database);
 mysql_query("set names 'utf8'");
 ?>
